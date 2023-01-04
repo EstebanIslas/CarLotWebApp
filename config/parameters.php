f6856e97460b5bddc5276ab3eaf9f31111202e09
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-01-04 11:22:58 -0600

    Agregando rutas para host virtual

--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,13 @@
 <?php
 
-define("base_url", "http://localhost/CarLotWebApp/"); #Define la ruta base del proyecto
+/*Define la ruta base 
+
+    host virtual: http://carlotweb.com/
+    local xampp: http://localhost/CarLotWebApp/
+    produccion: https://carlotweb.herokuapp.com/
+*/
+
+define("base_url", "http://carlotweb.com/"); #Define la ruta base del proyecto
 
 define('controller_default', 'IndexController'); #Controlador por defecto
 
